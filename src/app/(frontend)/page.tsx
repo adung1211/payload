7611e91cecdbcb516a3500efc5c7eca188aad23f
adung1211 <57287981+adung1211@users.noreply.github.com>
2025-05-12 16:29:44 +0700

    railway fix 2

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from 'next/navigation'
 
 export default function Home() {
   // Get the base URL dynamically (works for localhost, staging, or production)
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3006'
   // Redirect to [URL]/admin
-  redirect(`${baseUrl}/admin`)
+  redirect(`/admin`)
 }
